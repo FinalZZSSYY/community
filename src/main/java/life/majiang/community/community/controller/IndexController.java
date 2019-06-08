d78d128b7f22dde79f04b10f8c2b0563cffceb3e
FinalZZSSYY <980037227@qq.com>
2019-06-08 23:49:03 +0800

    add check cookies

--- src/main/java/life/majiang/community/community/controller/IndexController.java
+++ src/main/java/life/majiang/community/community/controller/IndexController.java
@@ -1,13 +1,34 @@
 package life.majiang.community.community.controller;
 
+import life.majiang.community.community.mapper.UserMapper;
+import life.majiang.community.community.pojo.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.annotation.Resource;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class IndexController {
 
+    @Resource
+    private UserMapper userMapper;
+
     @GetMapping("/")
-    public String index(){
+    public String index(HttpServletRequest request){
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie :cookies){
+            if(cookie.getName().equals("token")){
+                String token = cookie.getValue();
+                User user = userMapper.findByToken(token);
+                if (user != null){
+                    request.getSession().setAttribute("user", user);
+                }
+                break;
+            }
+        }
+
         return "index";
     }
 }
